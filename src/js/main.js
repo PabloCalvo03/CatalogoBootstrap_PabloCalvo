0603687747dd4ad95913b40ea0f12592791c85b3
Pablo <pablocalvoj@gmail.com>
2024-02-23 10:17:30 +0100

    improvement of performance

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,23 @@ import '../main.scss'
 
 import * as bootstrap from 'bootstrap';
 
+import "./toggle_theme.js"
+import "./show_toast.js"
+
 document.addEventListener('DOMContentLoaded', function () {
     const currentPage = document.documentElement.getAttribute('data-page');
     switch (currentPage) {
         case 'catalogo':
-            import('./show_toast.js')
             import('./quit_video.js')
-            import('./toggle_theme.js')
             break;
         case 'registro':
             import('./show_modal.js')
-            import('./show_toast.js')
             import('./show_tooltips.js')
             import('./validations.js')
             import('./dropdown_selection.js')
-            import('./toggle_theme.js')
             break;
         case 'estadisticas':
             import('./show_chart.js')
-            import('./show_toast.js')
-            import('./toggle_theme.js')
             break;
         default:
             break;
